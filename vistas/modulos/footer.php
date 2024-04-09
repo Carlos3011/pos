489aa2eb03b1489ca5a5920bab74f5f913ec54ca
Garcia <carlos.garcia.diaz2000@hotmail.com>
2024-04-08 23:49:17 -0600

    Primer commit

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,5 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2024 <a href="https://www.linkedin.com/in/carlos-garc%C3%ADa-diaz-fcc/">Carlos García Díaz</a>
+    </strong>
+    Todos los derechos reservados.
+</footer>
